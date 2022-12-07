7bf5f64b44d4ad61ec5cc26e2181b41410431cec
linvaldo_rodrigues <linvaldo.rodrigues10@gmail.com>
2022-12-07 08:27:09 -0300

    recupera registros

--- js/app.js
+++ js/app.js
@@ -38,6 +38,21 @@ class BancoDeDados {
 
     localStorage.setItem("id", id);
   }
+
+  recuperaRegistros() {
+    let despesas = Array();
+    let id = localStorage.getItem("id");
+
+    for (let i = 1; i <= id; i++) {
+      let despesa = JSON.parse(localStorage.getItem(i));
+      if (despesa == null) {
+        continue;
+      }
+      despesas.push(despesa);
+    }
+
+    return despesas;
+  }
 }
 let Bd = new BancoDeDados();
 
@@ -46,7 +61,7 @@ function cadastrarDespesa() {
   let mes = document.getElementById("mes");
   let dia = document.getElementById("dia");
   let tipo = document.getElementById("tipo");
-  let descricao = document.getElementById('descricao')
+  let descricao = document.getElementById("descricao");
   let valor = document.getElementById("valor");
 
   let despesa = new Despesa(
@@ -90,4 +105,36 @@ function cadastrarDespesa() {
 
     $("#modalRegistroDespesa").modal("show");
   }
+}
+
+function recuperaDespesas() {
+  let despesas = Array();
+  despesas = Bd.recuperaRegistros();
+
+  let listaDespesas = document.getElementById("listaDespesas");
+
+  despesas.forEach(function (d) {
+    let linha = listaDespesas.insertRow();
+    linha.insertCell(0).innerHTML = `${d.dia} / ${d.mes} / ${d.ano}`;
+    switch (d.tipo) {
+      case "1":
+        d.tipo = "Alimentação";
+        break;
+      case "2":
+        d.tipo = "Educação";
+        break;
+      case "3":
+        d.tipo = "Lazer";
+        break;
+      case "4":
+        d.tipo = "Saúde";
+        break;
+      case "5":
+        d.tipo = "transporte";
+        break;
+    }
+    linha.insertCell(1).innerHTML = d.tipo;
+    linha.insertCell(2).innerHTML = d.descricao;
+    linha.insertCell(3).innerHTML = `R$${d.valor}`;
+  });
 }
